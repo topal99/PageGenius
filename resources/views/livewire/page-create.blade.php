8b6252e76f142ee2e5ec49200511fbf965d62717
Topal <muhamamdtaufal@students.unnes.ac.id>
2025-07-30 14:51:47 +0700

    Fix multiple navbar in dashboard and /pages/create

--- resources/views/livewire/page-create.blade.php
+++ resources/views/livewire/page-create.blade.php
@@ -1,4 +1,3 @@
-<x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             Langkah 1: Pilih Template
@@ -23,4 +22,3 @@
             </div>
         </div>
     </div>
-</x-app-layout>
--- resources/views/livewire/user-dashboard.blade.php
+++ resources/views/livewire/user-dashboard.blade.php
@@ -1,4 +1,3 @@
-<x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Dashboard') }}
@@ -37,4 +36,3 @@
             </div>
         </div>
     </div>
-</x-app-layout>
